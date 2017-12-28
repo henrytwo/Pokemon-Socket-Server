98957fc38d3983b1c95c96256733530ed077f7b6
henrytwo <henryt.inc@gmail.com>
2017-12-28 18:07:58 -0500

    Start of engine

--- server.py
+++ server.py
@@ -12,147 +12,210 @@ from multiprocessing import *
 import random
 from math import *
 
-class attack:
-    def __init__(this, attack_data):
-        this.name = attack_data[0]
-        this.cost = int(attack_data[1])
-        this.damage = int(attack_data[2])
-        if len(attack_data) > 3:
-            this.special = attack_data[3]
+class room:
+    def __init__(self, code):
+        self.code = code
+        self.clients = []
+        self.game_running = True
+        self.shuffled = False
+
+        self.engine_thread = threading.Thread(target=self.engine)
+        self.engine_thread.start()
+
+    def engine(self):
+        while self.game_running:
+            try:
+                if not self.can_join():
+                    self.set_turn()
+
+                    print(self.clients)
+
+                    connections[self.clients[0]].make_action()
+                    connections[self.clients[1]].message("Waiting for sheit")
+
+
+            except:
+                self.game_running = False
+                for client in self.clients:
+                    connections[client].result("Engine Error")
+
+    def set_turn(self):
+
+        if not self.shuffled:
+            self.shuffled = True
+            random.shuffle(self.clients)
+
+        self.clients.append(self.clients[0])
+        del self.clients[0]
+
+    def can_join(self):
+        return len(self.clients) < 2
+
+    def join(self, uuid):
+        if len(self.clients) < 2:
+            self.clients.append(uuid)
+            return True
         else:
-            this.special = None
+            return False
+
+class attack:
+    def __init__(self, attack_data):
+        self.name = attack_data[0]
+        self.cost = int(attack_data[1])
+        self.damage = int(attack_data[2])
+        if len(attack_data) > 3:
+            self.special = attack_data[3]
+        else:
+            self.special = None
 
 class pokemon:
-    def __init__(this, pokemon_data_processed):
-        this.hp = pokemon_data_processed[0]
-        this.hptotal = pokemon_data_processed[0]
-        this.type = pokemon_data_processed[1]
-        this.resistance = pokemon_data_processed[2]
-        this.weakness = pokemon_data_processed[3]
-        this.numattacks = int(pokemon_data_processed[4])
-        this.attacks = this.get_attacks(int(pokemon_data_processed[4]), pokemon_data_processed[5:])
-        this.energy = 50
-        this.stunned = False
-        this.disabled = False
+    def __init__(self, pokemon_data_processed):
+        self.name = pokemon_data_processed[0]
+        self.hp = pokemon_data_processed[1]
+        self.hptotal = pokemon_data_processed[1]
+        self.type = pokemon_data_processed[2]
+        self.resistance = pokemon_data_processed[3]
+        self.weakness = pokemon_data_processed[4]
+        self.numattacks = int(pokemon_data_processed[5])
+        self.attacks = self.get_attacks(int(pokemon_data_processed[5]), pokemon_data_processed[6:])
+        self.energy = 50
+        self.stunned = False
+        self.disabled = False
 
-    def get_attacks(this, num_attacks, attack_data):
+    def get_attacks(self, num_attacks, attack_data):
         attacks = {}
         for num in range(0, num_attacks * 4, 4):
             attacks[attack_data[num]] = attack(attack_data[num : num + 3])
         return attacks
 
 class client:
-    def __init__(this, conn, addr):
-        this.alive = True
-        this.in_game = False
+    def __init__(self, conn, addr, client_id):
+        self.name = ''
+        self.pokemon_dict = {}
+        self.selected_pokemon = ''
+        self.client_id = client_id
 
-        this.in_queue = Queue()
-        this.out_queue = Queue()
+        self.alive = True
+        self.in_game = False
 
-        this.conn = conn
-        this.addr = addr
+        self.in_queue = Queue()
+        self.out_queue = Queue()
 
-        this.com_thread_read = threading.Thread(target=this.com_read, args=(conn,))
-        this.com_thread_read.start()
+        self.conn = conn
+        self.addr = addr
 
-        this.com_thread_write = threading.Thread(target=this.com_write, args=(conn,))
-        this.com_thread_write.start()
+        self.com_thread_read = threading.Thread(target=self.com_read, args=(conn,))
+        self.com_thread_read.start()
 
-        this.thread_service = threading.Thread(target=this.service)
-        this.thread_service.start()
+        self.com_thread_write = threading.Thread(target=self.com_write, args=(conn,))
+        self.com_thread_write.start()
+
+        self.thread_service = threading.Thread(target=self.service)
+        self.thread_service.start()
 
         print("Creating object for: %s:%i" % (addr[0], addr[1]))
 
-    def gen_code(this):
+    def gen_code(self):
         return str(uuid.uuid4())[0:8]
 
-    def pokemon_update(this):
-        return ""
+    def pokemon_update(self):
+        update_data = ''
+        for pokemon_name in self.pokemon_dict:
+            update_data += ' // %s // %s // %s' % (pokemon_name, self.pokemon_dict[pokemon_name].hp, self.pokemon_dict[pokemon_name].energy)
+        return update_data[4:]
 
-    def make_action(this):
-        this.out_queue.put("2 // MakeAction // %s" % this.pokemon_update())
-        return this.in_queue.get()
+    def make_action(self):
+        self.out_queue.put("2 // MakeAction // %s" % self.pokemon_update())
+        return self.in_queue.get()
 
-    def make_choose(this):
-        this.out_queue.put("2 // MakeChoose // %s" % this.pokemon_update())
-        return this.in_queue.get()
+    def make_choose(self):
+        self.out_queue.put("2 // MakeChoose // %s" % self.pokemon_update())
+        return self.in_queue.get()
 
-    def result(this, message):
-        this.out_queue.put("2 // Result // %s" % message)
+    def message(self, message):
+        self.out_queue.put("2 // Result // %s" % message)
+
+    def result(self, message):
+        self.out_queue.put("2 // Result // %s" % message)
         
-    def service(this):
-        while this.alive:
+    def service(self):
+        while self.alive:
             try:
-                if not this.in_game:
-                    code, message = this.com_get()
+                if not self.in_game:
+                    code, message = self.com_get()
 
                     # Establish communication
                     if code == 3000:
-                        this.out_queue.put('3000 // DOCTYPE!')
+                        self.out_queue.put('3000 // DOCTYPE!')
+                        self.name = message[0]
 
                     # Generate Gamecode
                     elif code == 0:
-                        game_code = this.gen_code()
-                        rooms[game_code] = {}
-                        this.out_queue.put('0 // %s' % game_code)
+                        game_code = self.gen_code()
+                        rooms[game_code] = room(game_code)
+                        self.out_queue.put('0 // %s' % game_code)
 
                     # Join game
                     elif code == 1:
                         if message[0] in rooms:  # Check if it's valid
-                            if len(rooms[message[0]]) > 1:
-                                this.out_queue.put('-1 // Error: Room Full')
-                            else:
-                                if len(message) >= 2:
+                            if rooms[message[0]].can_join():
+                                if len(message) == 1 + NUM_POKEMON:
+                                    for pokemon_name in message[1:]:
+                                        self.pokemon_dict[pokemon_name] = pokemon_data[pokemon_name]
 
-                                    client_id = str(uuid.uuid4())
-
-                                    rooms[message[0]][client_id] = {
-                                        'name': message[1],
-                                        'selected_pokemon': '',
-                                        'pokemon': get_pokemon(message[3:]),
-                                        'turn': not rooms[message[0]]
-                                    }
-
-                                    this.out_queue.put('1 // Success: Connected to room // %s' % client_id)
-
+                                    rooms[message[0]].join(self.client_id)
+                                    self.out_queue.put('1 // Success: Connected to room')
                                 else:
-                                    this.out_queue.put('-1 // Error: Invalid Data')
+                                    self.out_queue.put('-1 // Error: Invalid Data')
+                            else:
+                                self.out_queue.put('-1 // Error: Room Full')
                         else:
-                            this.out_queue.put('-1 // Error: Invalid Code')
+                            self.out_queue.put('-1 // Error: Invalid Code')
+
+                    # Preparing to enter game loop
+                    elif code == 2:
+                        if message[0] == 'InitPkmn':
+                            self.selected_pokemon = message[1]
+                            self.out_queue.put('2 // Success: Pokemon Registered')
+
+                        elif message[0] == 'Ready':
+                            self.make_action()
+
             except:
                 print(traceback.format_exc())
-                this.alive = False
+                self.alive = False
         try:
+            print("Client Disconnected")
             conn.send(bytes('-1 // Server Closed\r\n', 'utf-8'))
         except:
             pass
 
-    def com_read(this, conn):
-        while this.alive:
+    def com_read(self, conn):
+        while self.alive:
             try:
                 message_in = bytes.decode(conn.recv(1024), 'utf-8')
                 print('In:', message_in)
-                this.in_queue.put(message_in)
+                self.in_queue.put(message_in)
             except:
-                this.alive = False
+                self.alive = False
                 print(traceback.format_exc())
 
-    def com_write(this, conn):
-        while this.alive:
+    def com_write(self, conn):
+        while self.alive:
             try:
-                message_out = this.out_queue.get()
+                message_out = self.out_queue.get()
                 print('Out:', message_out)
                 conn.send(bytes(message_out + '\r\n', 'utf-8'))
             except:
-                this.alive = False
+                self.alive = False
                 print(traceback.format_exc())
 
-    def com_get(this):
+    def com_get(self):
         try:
-            data_in = this.in_queue.get()
+            data_in = self.in_queue.get()
 
             if data_in.count(' // ') == 0:
-                this.result('-1 // Error: Connection Error')
+                self.result('-1 // Error: Connection Error')
                 return -1
 
             data_list = data_in.strip('\n').split(' // ')
@@ -187,10 +250,14 @@ def logger(log_queue):
             data_file.write(str(log_queue.get()) + '\n')
             data_file.flush()
 
+def gen_uuid():
+    return str(uuid.uuid4())
+
 with open('data/config.rah', 'r') as config:
     config = config.read().strip().split("\n")
-    host = config[0]  # The ip address that the socket will bind to
-    port = int(config[1])  # The port that the socket will bind to
+    HOST = config[0]  # The ip address that the socket will bind to
+    PORT = int(config[1])  # The port that the socket will bind to
+    NUM_POKEMON = int(config[2])
 
 with open('data/pokemon_data.txt', 'r') as file:
     pokemon_data_raw = file.read().strip().split('\n')
@@ -207,22 +274,22 @@ with open('data/pokemon_data.txt', 'r') as file:
             except:
                 pass
 
-        pokemon_data[pokemon_data_processed[0]] = pokemon(pokemon_data_processed[1:])
+        pokemon_data[pokemon_data_processed[0]] = pokemon(pokemon_data_processed[0:])
 
 if __name__ == '__main__':
 
     rooms = {}
-    connections = []
+    connections = {}
 
-    port = random.randint(2000,3000)
+    PORT = random.randint(2000,3000)
 
     print('STARTING RASTERA POKEMON SERVER')
     print(' | rastera.xyz')
     print(' | SERIES F2017')
-    print('\nStarting server: %s:%i' % (host, port))
+    print('\nStarting server: %s:%i' % (HOST, PORT))
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Start TCP server
-    server.bind((host, port))
+    server.bind((HOST, PORT))
     server.listen(1000)
 
     #log_queue = Queue()
@@ -232,7 +299,9 @@ if __name__ == '__main__':
     # Creates client object for each incoming connection
     while True:
         try:
+            print(connections)
             conn, addr = server.accept()
-            connections.append(client(conn, addr))
+            client_id = gen_uuid()
+            connections[client_id] = client(conn, addr, client_id)
         except:
             pass
